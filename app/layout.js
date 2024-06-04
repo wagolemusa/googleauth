aa9898a6ef167bb3f6eb8e41c597566c368e8240
Your Name <you@example.com>
2024-06-04 16:57:08 +0300

    full project

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/Navbar";
+import { NextAuthProvider } from "./Providers";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,15 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <NextAuthProvider>
+      <div className="max-w-3xl mx-auto">
+        <Navbar />
+        {children}
+        </div>
+        </NextAuthProvider>
+      
+      </body>
     </html>
   );
 }
